1addc447b3fd239cf2cda28e8300fa9c49bc2c3c
Nicolas Oga <2112.oga@gmail.com>
2022-05-18 12:49:20 -0300

    Fix/improve aliases

--- profiles/main/aliases.sh
+++ profiles/main/aliases.sh
@@ -1,4 +1,4 @@
-alias ealiases='$EDITOR $DOTFILES/profiles/main/aliases.sh'
+alias ealiases='$EDITOR $0'
 alias edotfiles='$EDITOR $DOTFILES'
 alias egitignoreglobal='$EDITOR $DOTFILES/.gitignore_global'
 
@@ -6,6 +6,6 @@ alias ll='tree --dirsfirst -L 1 -ahv'
 alias bx='bundle exec'
 alias reload='source $DOTFILES/main.sh'
 
-alias install_dotfiles='sh $0'
+alias install_dotfiles='sh $DOTFILES/profiles/main/install.sh'
 
 alias edit="$EDITOR"
