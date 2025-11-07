1bf7b4bf6d9d2ad084bcb04ff3677fbaeda6c3ba
Nicolas Oga <2112.oga@gmail.com>
2025-11-07 17:09:45 -0300

    ZSH, fix asdf initiliazation

--- profiles/zsh/asdf.sh
+++ profiles/zsh/asdf.sh
@@ -1,1 +1,5 @@
-. /usr/local/opt/asdf/libexec/asdf.sh
+export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
+# append completions to fpath
+fpath=(${ASDF_DATA_DIR:-$HOME/.asdf}/completions $fpath)
+# initialise completions with ZSH's compinit
+autoload -Uz compinit && compinit
