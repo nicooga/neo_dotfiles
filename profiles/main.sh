eb3ed77c93f3e80d90514021db774daadb0ae287
Nicolas Oga <2112.oga@gmail.com>
2025-11-11 18:47:53 -0300

    Add small script for installing asdf if necessary

--- profiles/main.sh
+++ profiles/main.sh
@@ -3,3 +3,4 @@ source $DOTFILES/profiles/main/editor.sh
 source $DOTFILES/profiles/main/dotfiles_bin.sh
 source $DOTFILES/profiles/main/prompt.sh
 source $DOTFILES/profiles/main/tree.sh
+source $DOTFILES/profiles/main/asdf.sh
---
+++ profiles/main/asdf.sh
@@ -0,0 +1,3 @@
+if ! command -v asdf >/dev/null 2>&1; then
+    brew install asdf
+fi
