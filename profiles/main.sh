dae5224bee54b697db8c9239e893b1254e95ed89
Nicolas Oga <2112.oga@gmail.com>
2025-11-11 10:27:09 -0300

    Add installation script for tree command

--- profiles/main.sh
+++ profiles/main.sh
@@ -2,3 +2,4 @@ source $DOTFILES/profiles/main/aliases.sh
 source $DOTFILES/profiles/main/editor.sh
 source $DOTFILES/profiles/main/dotfiles_bin.sh
 source $DOTFILES/profiles/main/prompt.sh
+source $DOTFILES/profiles/main/tree.sh
---
+++ profiles/main/tree.sh
@@ -0,0 +1,3 @@
+if ! command -v tree >/dev/null 2>&1; then
+    brew install tree
+fi
